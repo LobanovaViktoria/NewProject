d238d64761ff29f36c335b5fc692d980c9abde08
Lobanova Viktoria <va_klevtsova@mail.ru>
2024-07-17 12:42:52 +0300

    create project

---
+++ NewProject/NewProject/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  NewProject
+//
+//  Created by Viktoria Lobanova on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
